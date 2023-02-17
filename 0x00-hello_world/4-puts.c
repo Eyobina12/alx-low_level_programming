385e5badd14ce2ed4d7bde87a6e99f2124ae74dd
Eyobina12 <eyobeyobina@yahoo,com>
2023-02-17 08:01:06 -0800

    team

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Programming is like building intersting for me to do it every day
+ * main - Programming is like building intersting for me to do it every day now i am coding too glad that i made it this far really happy for this
  * Return: 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle
+puts("/"Programming is like building a multilingual puzzle
 ");
 return (0);
 }
