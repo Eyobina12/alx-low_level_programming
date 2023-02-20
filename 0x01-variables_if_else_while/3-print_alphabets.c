07d4e9c1caf40d377e62ec3db47e775f45eabd39
Eyobina12 <eyobeyobina@yahoo,com>
2023-02-20 11:40:34 -0800

    print ABC

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+* main - Programming is like walking in forest intersting for me to do it
+*
+* Return: 0
+**/
+int main(void)
+{
+char c = ' ';
+char t;
 
+for (c = 'a' ; c <= 'z' ; c++)
+{
+putchar(tolower(c));
+}
+for (t = 'a' ; t <= 'z' ; t++)
+{
+putchar(toupper(t));
+}
+putchar('\n');
+return (0);
+}
