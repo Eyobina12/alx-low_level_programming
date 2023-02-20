da07153ca9f2bdd0972e7814a53fe3a4882b41cf
Eyobina12 <eyobeyobina@yahoo,com>
2023-02-20 12:02:17 -0800

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,20 @@
-
+#include <stdio.h>
+/**	
+ * main - hexadecimal prints all the numbers of base 16
+ * Return: 0 shows the success of the project
+ */
+int main(void)
+{
+int n;
+char c;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+}
+for (c = 'a'; c <= 'f'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
