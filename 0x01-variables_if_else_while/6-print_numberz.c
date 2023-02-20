3b87f48a486b3aea72084921be13fa3788840f2c
Eyobina12 <eyobeyobina@yahoo,com>
2023-02-20 11:51:28 -0800

    numberZ

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,16 @@
-
+#include <stdio.h>
+/**
+ * main - the strting point of the program tha prints the numberZ from 0-9
+ *
+ * Return: 0 returns the output
+ */
+int main(void)
+{
+int n;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
