730ea3ae0f2cccc4e314df8a0bad3ba82738b2d4
Eyobina12 <eyobeyobina@yahoo,com>
2023-02-20 11:55:27 -0800

    lowercase

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - prints the lowercase alphabet in reverse
+ * Return: 0 it shows the success of the program
+ */
+int main(void)
+{
+char c;
 
+for (c = 'z'; c >= 'a'; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
