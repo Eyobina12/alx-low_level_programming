899be7973a9fb1504b3a23ecd679d5c91218e4e5
Eyobina12 <eyobeyobina@yahoo,com>
2023-02-20 11:36:50 -0800

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - print an alphabet on lowercase
+ *
+ * Return:0 shows the success of the program
+ **/
+int main(void)
+{
+char c = ' ';
 
+for (c = 'a' ; c <= 'z' ; c++)
+{
+putchar(tolower(c));
+}
+putchar('\n');
+return (0);
+}
