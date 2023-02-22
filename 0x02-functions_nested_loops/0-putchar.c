2fcb5b81d788f8489cb9b7ee6f0fb9cf1c317666
Eyobina12 <eyobeyobina@yahoo,com>
2023-02-22 03:24:31 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
-#include “main.h”
+#include "main.h"
 /**
  * main – print _putchar
  * is wrriten by eyob12 now let us start cheking
- * Return: 0
+ * Return: 0 for me
 */
-Int main(void)
+int main(void)
 {
-_putchar(‘_’);
-_putchar(‘p’);
-_putchar(‘u’);
-_putchar(‘t’);
-_putchar(‘c’);
-_putchar(‘h’);
-_putchar(‘a’);
-_putchar(‘r’);
-_putchar(‘\n’);
+_putchar('_');
+_putchar ('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
