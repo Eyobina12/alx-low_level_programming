0348fcd3af44e2375b6d36f4a0a108da34d810f0
Eyobina12 <eyobeyobina@yahoo,com>
2023-04-04 06:36:08 -0700

    listint

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -29,5 +29,5 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
-listint_t *find_listint_loop(listint_t head);
+listint_t *find_listint_loop(listint_t *head);
 #endif
