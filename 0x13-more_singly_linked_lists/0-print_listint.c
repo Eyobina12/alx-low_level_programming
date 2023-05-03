fcaf83d369e2ef1471a9c59c6003657fc4857b5d
Eyobina12 <eyobeyobina@yahoo.com>
2023-05-03 07:24:11 -0700

    list print

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,12 +6,12 @@
  *
  * Return:nodes
  */
-size_t ptint_listint(const listint_t *h)
+size_t print_listint(const listint_t *h)
 {
-size_t num = 0:
+size_t num = 0;
 while (h)
 {
-printf("%d\n" ,h->n);
+printf("%d\n", h->n);
 num++;
 h = h->next;
 }
