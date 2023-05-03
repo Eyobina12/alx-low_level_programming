dd494559b87a0c817a1bfc155e2e550f627c40e1
Your Name <you@example.com>
2023-05-03 00:35:09 -0700

    head

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,19 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * linked list
+ * struct listint_s
+ * n:intger
+ * Descripition:linked list to the next line
+ */
+typedef struct listint_s
+{
+int n;
+struct listint_s *next
+listint_t
+}
+size_t print_listint(const listint_t *h);
+
